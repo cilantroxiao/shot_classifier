27fb9ff0e927c67dbc08702405c417157f59605c
Sandro Xiao <sandro@wustl.edu>
2026-03-27 11:20:56 -0400

    added edge density and face detector

--- classifier.py
+++ classifier.py
@@ -1,7 +1,9 @@
 import json
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import time 
 
 with open("training_labels_clean.json", "r", encoding="utf-8") as f:
     training_data = json.load(f)
@@ -11,33 +13,49 @@ labels_map = {
     "medium" : 1,
     "wide" : 2
 }
+def feature_extract(img):
+    root = os.getcwd()
+    data_folder = os.join(root, "/data/training_data/")
+    img_path = os.join(data_folder, img)
 
-#edge density
+    frame = cv.imread(img_path)
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    total_pixels = gray_frame.size
 
-#average brightness
-frame = cv2.imread('./data/training_data/11.jpg')
-gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-gray_max = 255.0
-avg_bright = np.mean(gray_frame) / gray_max #scale 0.0 to 1.0
-print(avg_bright)
-#display gray
-#cv2.imshow("Image", gray_frame)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    # display frame
+    '''
+    cv.imshow("Image", gray_frame)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    '''
+
+    # edge density
+    max_thres = 255
+    min_thres = 255 / 3
+    edges = cv.Canny(gray_frame, min_thres, max_thres)
+    edge_pixels = np.sum(edges == 255)
+    edge_density = edge_pixels / total_pixels
+    print(edge_density)
+
+    # average brightness
+    gray_max = 255.0
+    avg_bright = np.mean(gray_frame) / gray_max #scale 0.0 to 1.0
+    print(avg_bright)
+
+    # face detector
+    # https://github.com/informramiz/Face-Detection-OpenCV
+    haar_face_cascade = cv.CascadeClassifier("data/haarcascade_frontalface_alt.xml")
+    faces = haar_face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5)
+    print("Faces found: ", len(faces))
+    w = faces[0][2]
+    h = faces[0][3]
+    face_frac = w * h / total_pixels
+    print(face_frac)
+
+    return [edge_density, avg_bright, face_frac]
+
+X = []
+Y = []
 
 
-#face detector
-'''face_cascade = cv2.CascadeClassifier("./data/haarcascade.xml")
-#from geeksforgeeks.org
-def adjusted_detect_face(img):
-    face_img = img.copy()
-    face_rect = face_cascade.detectMultiScale(face_img, scaleFactor=1.2, minNeighbors=5)
 
-    for (x, y, w, h) in face_rect:
-        cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255), 10)
-
-    return face_img
-img = cv2.imread("./data/training_data/11.jpg")
-result = adjusted_detect_face(img)
-plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-plt.show()'''
